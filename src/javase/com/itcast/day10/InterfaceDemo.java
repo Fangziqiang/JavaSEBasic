5bc2cb5e79f7785951bf703d6bec897ba3f01bf9
fangziqiang <286330540@qq.com>
2019-07-01 21:40:13 +0800

    day10

--- src/javase/com/itcast/day10/InterfaceDemo.java
+++ src/javase/com/itcast/day10/InterfaceDemo.java
@@ -17,13 +17,133 @@ abstract class Person{
 	public String getName(){
 		return name;
 	}
-	public void setName(){
+	public void setName(String name){
 		this.name=name;
 	}
+	public int getAge(){
+		return age;
+	}
+	public void setAge(int age){
+		this.age=age;
+	}
 	
+	public void sleep(){
+		System.out.println("人都是要睡觉的");
+	}
+	public abstract void eat();
 }
 
+//定义运动员抽象类
+abstract class Player extends Person{
+	public Player(){}
+	public Player(String name,int age){
+		super(name,age);
+	}
+	
+	//学习
+	public abstract void study();
+}
+
+//定义教练抽象类
+abstract class Coach extends Person{
+	public Coach(){}
+	public Coach(String name,int age){
+		super(name,age);
+	}
+	
+	//教
+	public abstract void teach();
+}
+
+//定义乒乓球运动员具体类
+class PingPangPlayer extends Player implements SpeakEnglish{
+	public PingPangPlayer(){}
+	public PingPangPlayer(String name,int age){
+		super(name,age);
+	}
+	
+	public void eat(){
+		System.out.println("乒乓球运动员吃大白菜，喝小米粥");
+	}
+	public void study(){
+		System.out.println("乒乓球运动员学习如何发球和接球");
+	}
+	public void speak(){
+		System.out.println("乒乓球运动员说英语");
+	}
+}
+
+//定义乒乓球教练具体类
+class PingPangCoach extends Coach implements SpeakEnglish{
+	public PingPangCoach(){}
+	public PingPangCoach(String name,int age){
+		super(name,age);
+	}
+	
+	public void eat(){
+		System.out.println("乒乓球教练吃小白菜，喝大米粥");
+	}
+	public void teach(){
+		System.out.println("乒乓球教练教如何发球和接球");
+	}
+	public void speak(){
+		System.out.println("乒乓球教练说英语");
+	}
+}
+
+//定义篮球运动员具体类
+class BasketballPlayer extends Player{
+	public BasketballPlayer(){}
+	public BasketballPlayer(String name,int age){
+		super(name,age);
+	}
+	
+	public void eat(){
+		System.out.println("篮球运动员吃牛肉，喝牛奶");
+	}
+	public void study(){
+		System.out.println("篮球运动员学习如何运球和投篮");
+	}
+}
+//定义篮球教练具体类
+class BasketballCoach extends Coach{
+	public BasketballCoach(){}
+	public BasketballCoach(String name,int age){
+		super(name,age);
+	}
+	
+	public void eat(){
+		System.out.println("篮球教练吃羊肉，喝羊奶");
+	}
+	public void teach(){
+		System.out.println("篮球教练教如何运球和投篮");
+	}
+}
 
 public class InterfaceDemo {
+	public static void main(String[] args){
+		//测试运动员
+		PingPangPlayer ppp = new PingPangPlayer();
+		ppp.setAge(33);
+		ppp.setName("王浩");
+		System.out.println(ppp.getName()+"---"+ppp.getAge());
+		ppp.eat();
+		ppp.sleep();
+		ppp.study();
+		System.out.println( );
+		//通过带参构造给数据
+		
+		//篮球运动员
+		BasketballPlayer bp = new BasketballPlayer();
+		bp.setName("姚明");
+		bp.setAge(34);
+		System.out.println(bp.getName()+"---"+bp.getAge());
+		bp.eat();
+		bp.sleep();
+		bp.study();
+		
+		//测试教练
+		
+	}
 
 }
---
+++ src/javase/com/itcast/day10/StudentTest.java
@@ -0,0 +1,40 @@
+package com.itcast.day10;
+
+/*
+ * 形式参数：
+ * 		基本类型（太简单，不是我今天要讲解的）
+ * 		引用类型
+ * 			类：（匿名对象的时候其实我们已经讲过了）需要的是该类的对象
+ * 			抽象类
+ * 			接口
+ */		
+
+class Student{
+	public void study(){
+		System.out.println("Good Good Study,Day Day up");
+	}
+}
+
+class StudentDemo{
+	public void method(Student s){
+		s.study();
+	}
+}
+public class StudentTest {
+	public static void main(String[] args){
+		//需求：我要测试学生类的study()方法
+		Student s = new Student();
+		s.study();
+		System.out.println("------------------");
+		
+		//需求2：我要测试studentDemo类中的method()方法
+		StudentDemo sd =new StudentDemo();
+		Student ss = new Student();
+		sd.method(ss);
+		System.out.println("------------------");
+		
+		//匿名对象用法
+		new StudentDemo().method(new Student());
+	}
+
+}
